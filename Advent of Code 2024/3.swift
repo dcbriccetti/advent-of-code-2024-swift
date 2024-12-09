8f8dc8b150823468865213450ff5960996d49e09
Dave Briccetti <daveb@davebsoft.com>
2024-12-08 18:10:50 -0800

    Day 3 part 2

--- Advent of Code 2024/3.swift
+++ Advent of Code 2024/3.swift
@@ -1,7 +1,21 @@
-func day3() throws {
-    let data = try String(contentsOfFile: path + "/3-test.txt", encoding: .utf8)
-    let sum: Int = data.matches(of: /mul\((\d+)\,(\d+)\)/)
-        .map { Int($0.1)! * Int($0.2)! }
-        .reduce(0, +)
-    print("Sum of products: \(sum)")
+func day3(for part: Part) throws {
+    let data = try String(contentsOfFile: path + "/3.txt", encoding: .utf8)
+    let matches = data.matches(of: /mul\((\d+)\,(\d+)\)|(do\(\))|(don't\(\))/)
+    var enabled = true
+
+    let sum = matches.compactMap { match -> Int? in
+        if match.0.starts(with: "don") && part == .part2 {
+            enabled = false
+            return nil
+        } else if match.0.starts(with: "do") {
+            enabled = true
+            return nil
+        } else if enabled && match.0.starts(with: "mul") {
+            return Int(match.1!)! * Int(match.2!)!
+        } else {
+            return nil
+        }
+    }.reduce(0, +)
+
+    print(sum)
 }
--- Advent of Code 2024/main.swift
+++ Advent of Code 2024/main.swift
@@ -1,1 +1,2 @@
-try day3()
+try day3(for: .part1)
+try day3(for: .part2)
