e1a6667dc59ac063e23855364bbe2d344ee7493c
Dave Briccetti <daveb@davebsoft.com>
2024-12-08 18:36:06 -0800

    Improve day 3 code

--- Advent of Code 2024/3.swift
+++ Advent of Code 2024/3.swift
@@ -1,20 +1,18 @@
 func day3(for part: Part) throws {
-    let data = try String(contentsOfFile: path + "/3.txt", encoding: .utf8)
+    let data = try String(contentsOfFile: path + "/3-test.txt", encoding: .utf8)
     let matches = data.matches(of: /mul\((\d+)\,(\d+)\)|(do\(\))|(don't\(\))/)
-    var enabled = true
-
+    var insideDont = false
     let sum = matches.compactMap { match -> Int? in
-        if match.0.starts(with: "don") && part == .part2 {
-            enabled = false
-            return nil
-        } else if match.0.starts(with: "do") {
-            enabled = true
-            return nil
-        } else if enabled && match.0.starts(with: "mul") {
+        let includeThisMul = part == .part1 || !insideDont
+        if includeThisMul && match.0.starts(with: "mul") {
             return Int(match.1!)! * Int(match.2!)!
-        } else {
-            return nil
         }
+        if match.0.starts(with: "don't(") {
+            insideDont = true
+        } else if match.0.starts(with: "do(") {
+            insideDont = false
+        }
+        return nil
     }.reduce(0, +)
 
     print(sum)
