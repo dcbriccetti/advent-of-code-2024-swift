22397aa6c4dadaa5d2f8288a096d4fe26c4d23aa
Dave Briccetti <daveb@davebsoft.com>
2024-12-09 22:04:08 -0800

    Day 5, part 2

--- Advent of Code 2024/5.swift
+++ Advent of Code 2024/5.swift
@@ -1,38 +1,40 @@
 func day5(for part: Part) throws {
+    let (prerequisitesByPageNum, updates) = try getData()
+
+    func prerequisitesCount(for pageNum: String, in update: [String]) -> Int {
+        prerequisitesByPageNum[pageNum, default: []].filter(update.contains).count
+    }
+
+    let totalMiddlePageNumbers = updates.reduce(0) { (total, update) in
+        let allPrereqs: [(String, Int)] = update.map { pageNum in
+            (pageNum, prerequisitesCount(for: pageNum, in: update))
+        }
+        let sortedPrereqs = allPrereqs.sorted { $0.1 < $1.1 }
+        let pageNumbersInOrder = sortedPrereqs.map(\.0)
+        let updateOk = pageNumbersInOrder == update
+        if (updateOk && part == .part1) || (!updateOk && part == .part2) {
+            let sequence = part == .part1 ? update : pageNumbersInOrder
+            if let middlePageNumber = Int(sequence[sequence.count / 2]) {
+                return total + middlePageNumber
+            }
+        }
+        return total
+    }
+    print(totalMiddlePageNumbers)
+}
+
+func getData() throws -> (prerequisitesByPageNum: [String : Set<String>], updates: [[String]]) {
     let data = try String(contentsOfFile: path + "/5.txt", encoding: .utf8)
     let sections = data.split(separator: "\n\n")
-    let prereqs: [[String]] = sections[0].split(separator: "\n").map { line in
-        line.split(separator: "|").map { update in String(update)}
-    }
-    let prerequisitesByPageNum = prereqs.reduce(into: [String: Set<String>]()) { (dict, prereq) in
-        dict[prereq[1], default: []].insert(prereq[0])
-    }
+    let prerequisitesByPageNum: [String : Set<String>] = sections[0].split(separator: "\n")
+        .map { line in line.split(separator: "|").map { String($0) } }
+        .reduce(into: [String: Set<String>]()) { (dict, prereq_pair) in
+            let pageNum: String = prereq_pair[1]
+            let mustPrecedePageNum: String = prereq_pair[0]
+            dict[pageNum, default: []].insert(mustPrecedePageNum)
+        }
     let updates: [[String]] = sections[1].split(separator: "\n").map { section in
-        section.split(separator: ",").map { update in String(update)}
+        section.split(separator: ",").map { String($0) }
     }
-    let totalMiddlePageNumbers = updates.reduce(0) { (acc, update) in
-        let updatePages = Set<String>(update)
-        var encounteredPageNumbers = Set<String>()
-        print(update)
-        var updateOk = true
-        for pageNum in update {
-            if let pagePrereqs = prerequisitesByPageNum[pageNum] {
-                let relevantPrereqs = pagePrereqs.filter(updatePages.contains)
-                if !relevantPrereqs.allSatisfy(encounteredPageNumbers.contains) {
-                    updateOk = false
-                }
-            }
-            encounteredPageNumbers.insert(pageNum)
-        }
-        print(updateOk ? "OK" : "NOT OK")
-        if updateOk {
-            let middleIndex: Int = update.count / 2
-            let middlePageNumber = Int(update[middleIndex])!
-            return acc + middlePageNumber
-        } else {
-            return acc
-        }
-    }
-    
-    print(totalMiddlePageNumbers)
+    return (prerequisitesByPageNum, updates)
 }
--- Advent of Code 2024/main.swift
+++ Advent of Code 2024/main.swift
@@ -1,2 +1,2 @@
 try day5(for: .part1)
-//try day5(for: .part2)
+try day5(for: .part2)
