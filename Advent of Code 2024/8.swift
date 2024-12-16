bee9f5414119ef1f99a92d66106bb2915c2b8789
Dave Briccetti <daveb@davebsoft.com>
2024-12-15 21:47:28 -0800

    Improve readability of Day 8

--- Advent of Code 2024/8.swift
+++ Advent of Code 2024/8.swift
@@ -4,7 +4,7 @@ func day8(for part: Part) throws {
     let numRows = lines.count
     let numCols = lines[0].count
 
-    let groupedNodes: [Character: [Vector]] = Dictionary(grouping: lines.enumerated().flatMap { (rowIndex, line) in
+    let antennaGroups: [Character: [Vector]] = Dictionary(grouping: lines.enumerated().flatMap { (rowIndex, line) in
         Array(line).enumerated()
             .compactMap { (colIndex, cell) in
                 cell == "." ? nil : (cell, Vector(y: rowIndex, x: colIndex))
@@ -12,36 +12,36 @@ func day8(for part: Part) throws {
     }, by: { $0.0 })
         .mapValues { $0.map { $0.1 } }
 
-    func makeAntinodes(p1: Vector, p2: Vector) -> [Vector] {
-        let Δ12 = Vector(y: p2.y - p1.y, x: p2.x - p1.x)
+    func makeAntinodes(antenna1: Vector, antenna2: Vector) -> [Vector] {
+        let Δ12 = Vector(y: antenna2.y - antenna1.y, x: antenna2.x - antenna1.x)
         let Δ21 = -Δ12
         if part == .part1 {
-            return [p1 - Δ12, p2 + Δ12]
+            return [antenna1 - Δ12, antenna2 + Δ12]
         }
 
         return [Δ12, Δ21].flatMap { Δ in
-            (0...).lazy.map { p1 + Δ * Vector(y: $0, x: $0) }
+            (0...).lazy.map { antenna1 + Δ * Vector(y: $0, x: $0) }
                 .prefix { $0.validIn(height: numRows, width: numCols) }
         }
     }
 
-    let antinodes: [Vector] = groupedNodes.flatMap { (_, coordPairs) in
-        let allPairs: [(Vector, Vector)] = coordPairs.indices.flatMap { i in
-            coordPairs[(i + 1)...].map { (coordPairs[i], $0) }
+    let antinodeLocations: [Vector] = antennaGroups.flatMap { (_, locations) in
+        let allAntennaPairs: [(Vector, Vector)] = locations.indices.flatMap { i in
+            locations[(i + 1)...].map { (locations[i], $0) }
         }
 
-        return allPairs.flatMap { makeAntinodes(p1: $0.0, p2: $0.1) }
+        return allAntennaPairs.flatMap { makeAntinodes(antenna1: $0.0, antenna2: $0.1) }
     }
 
     func displayAntinodes(_ antinodes: [Vector]) {
-        let output = (0..<numRows).map { r in
+        let display = (0..<numRows).map { r in
             (0..<numCols).map { c in
                 antinodes.contains(Vector(y: r, x: c)) ? "#" : "."
             }.joined()
         }.joined(separator: "\n")
-        print(output)
+        print(display)
     }
 
-    print(Set(antinodes).count)
-    displayAntinodes(antinodes)
+    print(Set(antinodeLocations).count)
+    displayAntinodes(antinodeLocations)
 }
